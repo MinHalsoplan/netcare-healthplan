815e3e84b5fd7571172fc407d77b85d647ec100e
PeterLarsson <peter.larsson@callistaenterprise.se>
2012-01-06 11:46:12 +0100

    test corrected

--- netcare-core/src/test/java/org/callistasoftware/netcare/core/spi/HealthPlanServiceTest.java
+++ netcare-core/src/test/java/org/callistasoftware/netcare/core/spi/HealthPlanServiceTest.java
@@ -320,7 +320,9 @@ public class HealthPlanServiceTest extends TestSupport {
 		ActivityDefinitionEntity ad = createActivityDefinitionEntity();
 		ServiceResult<PatientEvent> sr = service.getActualEventsForPatient(PatientBaseViewImpl.newFromEntity(ad.getHealthPlan().getForPatient()));
 		PatientEvent event = sr.getData();
-		assertEquals(4, event.getDueReports());
-		assertEquals(2, event.getNumReports());
+		assertEquals(true, event.getDueReports() > 0);
+		Calendar cal = Calendar.getInstance();
+		int day = cal.get(Calendar.DAY_OF_WEEK);
+		assertEquals(true, (event.getNumReports() > 0 && (day == 2 || day == 5)) || event.getNumReports() == 0);
 	}
 }
