d19dcd51973a6177482fc1afbe4f635efa83bb1a
PeterLarsson <peter.larsson@callistaenterprise.se>
2012-01-10 14:49:18 +0100

    alarm task implemented

--- netcare-core/src/main/java/org/callistasoftware/netcare/core/job/SystemAlarmJob.java
+++ netcare-core/src/main/java/org/callistasoftware/netcare/core/job/SystemAlarmJob.java
@@ -16,10 +16,24 @@
  */
 package org.callistasoftware.netcare.core.job;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
+import org.callistasoftware.netcare.core.repository.AlarmRepository;
+import org.callistasoftware.netcare.core.repository.HealthPlanRepository;
+import org.callistasoftware.netcare.core.repository.ScheduledActivityRepository;
+import org.callistasoftware.netcare.core.spi.impl.HealthPlanServiceImpl;
+import org.callistasoftware.netcare.model.entity.AlarmCause;
+import org.callistasoftware.netcare.model.entity.AlarmEntity;
+import org.callistasoftware.netcare.model.entity.HealthPlanEntity;
+import org.callistasoftware.netcare.model.entity.ScheduledActivityEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,6 +43,15 @@ public class SystemAlarmJob {
 
 	private static Logger log = LoggerFactory.getLogger(SystemAlarmJob.class);
 	
+	@Autowired
+	private ScheduledActivityRepository saRepo;
+	
+	@Autowired
+	private HealthPlanRepository hpRepo;
+	
+	@Autowired
+	private AlarmRepository alRepo;
+
 	@PostConstruct
 	public void init() {
 		this.run();
@@ -38,7 +61,49 @@ public class SystemAlarmJob {
 	@Transactional
 	public void run() {
 		log.info("======== ALARM JOB STARTED =========");
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -(HealthPlanServiceImpl.SCHEMA_HISTORY_DAYS));
+		cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+		cal.add(Calendar.DATE, -1);
 		
+		plans(cal.getTime());
+		
+		activities(cal.getTime());
+		
+
 		log.info("======== ALARM JOB COMPLETED =========");
+	}
+	
+	//
+	private void activities(Date endDate) {
+		List<ScheduledActivityEntity> sal = saRepo.findByScheduledTimeLessThanAndReportedTimeIsNull(endDate);
+		List<AlarmEntity> al = new LinkedList<AlarmEntity>();
+		List<ScheduledActivityEntity> saSave = new LinkedList<ScheduledActivityEntity>();
+		for (ScheduledActivityEntity sae : sal) {
+			al.add(AlarmEntity.newEntity(AlarmCause.UNREPORTED_ACTIVITY, 
+					sae.getActivityDefinitionEntity().getHealthPlan().getCareUnit().getHsaId(), 
+					sae.getId()));
+			sae.setRejected(true);
+			sae.setReportedTime(new Date());
+			sae.setNote("Stängd automatiskt.");
+			sae.setActualValue(0);
+			saSave.add(sae);
+		}
+		if (al.size() > 0) {
+			alRepo.save(al);
+			saRepo.save(saSave);
+		}		
+	}
+	
+	//
+	private void plans(Date endDate) {
+		List<HealthPlanEntity> hpl = hpRepo.findByEndDateLessThan(endDate);
+		List<AlarmEntity> al = new LinkedList<AlarmEntity>();
+		for (HealthPlanEntity hpe : hpl) {
+			al.add(AlarmEntity.newEntity(AlarmCause.PLAN_EXPIRES, hpe.getCareUnit().getHsaId(), hpe.getId()));
+		}
+		if (al.size() > 0) {
+			alRepo.save(al);
+		}		
 	}
 }
--- netcare-core/src/main/java/org/callistasoftware/netcare/core/spi/impl/AlarmServiceImpl.java
+++ netcare-core/src/main/java/org/callistasoftware/netcare/core/spi/impl/AlarmServiceImpl.java
@@ -17,102 +17,22 @@
 package org.callistasoftware.netcare.core.spi.impl;
 
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-
 import org.callistasoftware.netcare.core.api.Alarm;
 import org.callistasoftware.netcare.core.api.ServiceResult;
 import org.callistasoftware.netcare.core.api.impl.ServiceResultImpl;
 import org.callistasoftware.netcare.core.api.messages.EntityNotFoundMessage;
-import org.callistasoftware.netcare.core.repository.AlarmRepository;
 import org.callistasoftware.netcare.core.repository.CareUnitRepository;
-import org.callistasoftware.netcare.core.repository.HealthPlanRepository;
-import org.callistasoftware.netcare.core.repository.ScheduledActivityRepository;
 import org.callistasoftware.netcare.core.spi.AlarmService;
-import org.callistasoftware.netcare.model.entity.AlarmCause;
-import org.callistasoftware.netcare.model.entity.AlarmEntity;
 import org.callistasoftware.netcare.model.entity.CareUnitEntity;
-import org.callistasoftware.netcare.model.entity.HealthPlanEntity;
-import org.callistasoftware.netcare.model.entity.ScheduledActivityEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AlarmServiceImpl extends ServiceSupport implements AlarmService {
-
-	private static Logger log = LoggerFactory.getLogger(AlarmServiceImpl.class);
-	
 	@Autowired
 	private CareUnitRepository cuRepo;
 	
-	@Autowired
-	private ScheduledActivityRepository saRepo;
-	
-	@Autowired
-	private HealthPlanRepository hpRepo;
-	
-	@Autowired
-	private AlarmRepository alRepo;
-	
-	@PostConstruct
-	public void initialize() {
-		this.run();
-	}
-	
-	@Override
-	public void run() {
-		log.info("======== ALARM JOB STARTED =========");
-		
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DATE, -(HealthPlanServiceImpl.SCHEMA_HISTORY_DAYS));
-		cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-		cal.add(Calendar.DATE, -1);
-		
-		plans(cal.getTime());
-		
-		activities(cal.getTime());
-		
-		
-		log.info("======== ALARM JOB COMPLETED =========");
-	}
-	
-	private void activities(Date endDate) {
-		List<ScheduledActivityEntity> sal = saRepo.findByScheduledTimeLessThanAndReportedTimeIsNull(endDate);
-		List<AlarmEntity> al = new LinkedList<AlarmEntity>();
-		List<ScheduledActivityEntity> saSave = new LinkedList<ScheduledActivityEntity>();
-		for (ScheduledActivityEntity sae : sal) {
-			al.add(AlarmEntity.newEntity(AlarmCause.UNREPORTED_ACTIVITY, 
-					sae.getActivityDefinitionEntity().getHealthPlan().getCareUnit().getHsaId(), 
-					sae.getId()));
-			sae.setRejected(true);
-			sae.setReportedTime(new Date());
-			sae.setNote("Stängd automatiskt.");
-			sae.setActualValue(0);
-			saSave.add(sae);
-		}
-		if (al.size() > 0) {
-			alRepo.save(al);
-			saRepo.save(saSave);
-		}		
-	}
-	
-	private void plans(Date endDate) {
-		List<HealthPlanEntity> hpl = hpRepo.findByEndDateLessThan(endDate);
-		List<AlarmEntity> al = new LinkedList<AlarmEntity>();
-		for (HealthPlanEntity hpe : hpl) {
-			al.add(AlarmEntity.newEntity(AlarmCause.PLAN_EXPIRES, hpe.getCareUnit().getHsaId(), hpe.getId()));
-		}
-		if (al.size() > 0) {
-			alRepo.save(al);
-		}		
-	}
 	
 	@Transactional
 	@Override
