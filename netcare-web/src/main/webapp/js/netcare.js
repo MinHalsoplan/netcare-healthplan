f0eddae474a150a881b0e0b8131de359bf5c53ae
krantz.marcus@gmail.com <krantz.marcus@gmail.com>
2012-01-12 18:35:44 +0100

    Fixed fonts for jquery ui elements.

--- netcare-web/src/main/webapp/js/netcare.js
+++ netcare-web/src/main/webapp/js/netcare.js
@@ -24,6 +24,9 @@ $(function() {
 	 * Bind all autocomplete boxes
 	 */
 	console.log("Bind autocomplete fields...");
+	
+	var spinner = $('<span>').addClass('spinner');
+	
 	$('.nc-autocomplete').autocomplete({
 		search : function(event, ui) {
 			$(this).addClass('spinner');
