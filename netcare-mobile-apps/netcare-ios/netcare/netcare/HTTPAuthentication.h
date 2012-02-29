22848c5145cbffd0c68437614f388e727e3c26a4
PeterLarsson <peter.larsson@callistaenterprise.se>
2012-02-29 15:21:42 +0100

    header updated

--- netcare-mobile-apps/netcare-ios/netcare/netcare/HTTPAuthentication.h
+++ netcare-mobile-apps/netcare-ios/netcare/netcare/HTTPAuthentication.h
@@ -2,9 +2,21 @@
 //  HTTPAuthentication.h
 //  netcare
 //
-//  Created by Peter Larsson on 2012-02-28.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+// Copyright (C) 2012 Callista Enterprise AB <info@callistaenterprise.se>
 //
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 
 #import <Foundation/Foundation.h>
 #import "HTTPConnection.h"
