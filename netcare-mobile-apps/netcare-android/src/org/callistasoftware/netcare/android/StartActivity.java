bc2f438f328a981535c66411e6280e0ba0e0474d
Marcus Krantz <krantz.marcus@gmail.com>
2013-01-15 10:21:39 +0100

    Initial work on bankid

--- netcare-mobile-apps/netcare-android/src/org/callistasoftware/netcare/android/StartActivity.java
+++ netcare-mobile-apps/netcare-android/src/org/callistasoftware/netcare/android/StartActivity.java
@@ -1,22 +1,12 @@
 package org.callistasoftware.netcare.android;
 
-import org.apache.http.HttpResponse;
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
-import android.content.SharedPreferences.Editor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.webkit.WebViewDatabase;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 /**
  * Start activity for the netcare app. Prompts for username and
@@ -26,11 +16,6 @@ import android.widget.Toast;
  *
  */
 public class StartActivity extends Activity {
-    
-	private static final String TAG = StartActivity.class.getSimpleName();
-	
-	private EditText cnr;
-	private EditText pin;
 	
 	private Button login;
 	
@@ -38,75 +23,22 @@ public class StartActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        final boolean error = getIntent().getBooleanExtra("error", false);
-        
-        /*
-         * Reset the credentials database
-         */
-        Log.d(TAG, "Clearing credentials...");
-        WebViewDatabase.getInstance(getApplicationContext()).clearHttpAuthUsernamePassword();
-        
         setContentView(R.layout.start);
-        
-        if (error) {
-        	final String errorMessage = getIntent().getStringExtra("errorMessage");
-        	
-        	final AlertDialog.Builder builder = new AlertDialog.Builder(StartActivity.this);
-			builder.setMessage(errorMessage)
-			.setCancelable(false)
-			.setTitle(getString(R.string.error))
-			.setPositiveButton("OK", new OnClickListener() {
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					dialog.cancel();
-				}
-			});
-			
-			builder.create().show();
-        }
-        
-        this.cnr = (EditText) this.findViewById(R.id.cnr);
-        this.pin = (EditText) this.findViewById(R.id.pin);
         
         this.login = (Button) this.findViewById(R.id.loginButton);
         this.login.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				Log.i(TAG, "Logging in user " + cnr.getText().toString());
-				final String username = cnr.getText().toString().trim();
-				final String password = pin.getText().toString().trim();
-				
-				if (ApplicationUtil.isWhitespace(username) || ApplicationUtil.isWhitespace(password)) {
-					Log.d(TAG, "Display toast to inform user that blanks are not allowed.");
-					Toast.makeText(StartActivity.this.getApplicationContext(), getString(R.string.provideCredentials), 5000).show();
-					return;
-				}
-				
-				new LoginTask(v.getContext(), new ServiceCallback<HttpResponse>() {
-					
-					@Override
-					public void onSuccess(HttpResponse response) {
-						/*
-						 * Save credentials
-						 */
-						Log.d(TAG, "Storing user / pin for user.");
-						final Editor edit = PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit();
-						edit.putString("crn", username);
-						edit.putString("pin", password);
-						edit.commit();
-						
-						startActivity(new Intent(StartActivity.this, WebViewActivity.class));
-					}
-					
-					@Override
-					public void onFailure(String reason) {
-						Log.d(TAG, "Display toast to inform user that blanks are not allowed.");
-						Toast.makeText(StartActivity.this.getApplicationContext(), getString(R.string.credentialsError), 5000).show();
-						return;
-					}
-				}).execute(username, password);
+				Intent intent = new Intent();
+				intent.setPackage("com.bankid.bus");
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.addCategory(Intent.CATEGORY_BROWSABLE); //optional
+				intent.addCategory(Intent.CATEGORY_DEFAULT); //optional
+				intent.setType("bankid");            
+				intent.setData(Uri.parse("bankid://www.bankid.com?redirect=null"))
+				;
+				startActivityForResult(intent, 0);
 			}
 		});
     }
