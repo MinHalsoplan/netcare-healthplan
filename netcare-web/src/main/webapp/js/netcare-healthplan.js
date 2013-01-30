20fae7dbefe3445ab35e6bf413ef0ca3e2e563dd
Marcus Krantz <krantz.marcus@gmail.com>
2013-01-30 09:56:41 +0100

    Fixed bug in activity template processing.

--- netcare-web/src/main/webapp/js/netcare-healthplan.js
+++ netcare-web/src/main/webapp/js/netcare-healthplan.js
@@ -2504,6 +2504,7 @@ var NC_MODULE = {
 				if (activity.actTime) {
 					$('#' + activity.id + '-report-time').val(activity.actTime);
 				} else {
+					var d = new Date();
 					var min = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
 					var timeString = d.getHours() + ':' + min;
 					$('#' + activity.id + '-report-time').val(timeString);
