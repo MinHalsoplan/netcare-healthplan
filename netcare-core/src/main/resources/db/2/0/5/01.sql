85a33913457cb1920087a051a6583be01e3c3b5d
Marcus Krantz <krantz.marcus@gmail.com>
2013-04-10 09:38:21 +0200

    Added liquibase scripts for the 2.0.5 upgrade.

--- netcare-core/src/main/resources/db/2/0/5/01.sql
+++
@@ -1,8 +0,0 @@
-/* TODO - Make sure this is the right way to add the column */
-alter table nc_health_plan add column active boolean not null;
-
-update nc_health_plan set active=false where archived=true;
-update nc_health_plan set active=true where archived=false or archived is null;
-
-alter table nc_health_plan drop column archived;
-alter table nc_health_plan drop column iteration;
